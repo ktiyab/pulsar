a31852826485e67df7a8fed4031d921f7105fb6d
Tiyab <1818065+ktiyab@users.noreply.github.com>
2022-05-10 14:45:05 +0200

    Correct Secret Manager section loop

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -74,34 +74,35 @@ then
   echo
   if [[ $REPLY =~ ^[Yy]$ ]]
   then
-      # Root folder
-      cd ..
-      echo "---> Creating the Cloud functions ZIP file..."
+    pwd
+    # Root folder
+    cd ..
+    echo "---> Creating the Cloud functions ZIP file..."
 
-      # Removing existing zip
-      rm $PULSAR_ZIP
-      cd "$PULSAR_FOLDER"
+    # Removing existing zip
+    rm $PULSAR_ZIP
+    cd "$PULSAR_FOLDER"
 
-      # Write new version date
-      echo 'Pulsar build of: '$(date +"%m/%d/%Y") > README.txt
-      zip -r "../$PULSAR_ZIP" *
+    # Write new version date
+    echo 'Pulsar build of: '$(date +"%m/%d/%Y") > README.txt
+    zip -r "../$PULSAR_ZIP" *
 
-      # --  --  --  --  --  -- A1 - Create buckets if not exist --  --  --  --  --  -- --  --  --  --  --  --
-      echo "---> Creating the default GCS bucket $PULSAR_BUCKET_NAME if not exist..."
-      # Root folder
-      cd ..
-      gsutil mb -l $REGION $PULSAR_BUCKET_NAME_PATH
+    # --  --  --  --  --  -- A1 - Create buckets if not exist --  --  --  --  --  -- --  --  --  --  --  --
+    echo "---> Creating the default GCS bucket $PULSAR_BUCKET_NAME if not exist..."
+    # Root folder
+    cd ..
+    gsutil mb -l $REGION $PULSAR_BUCKET_NAME_PATH
 
-      # Copy function to bucket
-      echo "---> Trying to delete existing cloud function zip in the bucket... "
-      gsutil rm $PULSAR_GCS_SOURCE_PATH
-      echo "---> Copying the cloud function to the bucket..."
-      gsutil cp "$PULSAR_ZIP" "$PULSAR_BUCKET_NAME_PATH"
+    # Copy function to bucket
+    echo "---> Trying to delete existing cloud function zip in the bucket... "
+    gsutil rm $PULSAR_GCS_SOURCE_PATH
+    echo "---> Copying the cloud function to the bucket..."
+    gsutil cp "$PULSAR_ZIP" "$PULSAR_BUCKET_NAME_PATH"
 
-      # Moving cursor to original position
-      cd ./scripts
+    # Moving cursor to original position
+    cd ./scripts
   else
-      echo "---> Using existing Cloud Storage configurations and files"
+    echo "---> Using existing Cloud Storage configurations and files"
   fi
 
   #--  --  --  --  --  -- B -  Deploy PubSub topic --  --  --  --  --  -- --  --  --  --  --  -- --  --  --  --
@@ -109,12 +110,13 @@ then
   echo
   if [[ $REPLY =~ ^[Yy]$ ]]
   then
-      #--  --  --  -- Creating topic
-      echo "---> Creating topic if not exist $PULSAR_TOPIC"
-      gcloud pubsub topics create "$PULSAR_TOPIC"
-      echo "---> The pulsar topic is created"
+    pwd
+    #--  --  --  -- Creating topic
+    echo "---> Creating topic if not exist $PULSAR_TOPIC"
+    gcloud pubsub topics create "$PULSAR_TOPIC"
+    echo "---> The pulsar topic is created"
   else
-      echo "---> Using existing topic"
+    echo "---> Using existing topic"
   fi
 
   #--  --  --  --  --  -- C -  Deploy Secret Manager files --  --  --  --  --  -- --  --  --  --  --  -- --  --
@@ -122,61 +124,65 @@ then
   echo
   if [[ $REPLY =~ ^[Yy]$ ]]
   then
+    pwd
     # -- -- -- C1 - Create new secrets if exist
     read -p "--->>>> Do you want to create new secrets in Secret Manager? [Y/y or N/n]: " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
-          echo "---> Creating non existing secrets"
-          # -- -- Loop secret folder and load files content into secret manager
-          for filename in ./$PULSAR_SECRETS_FOLDER/*$PULSAR_SECRETS_EXT; do
-              [ -e "$filename" ] || continue
+      pwd
+      echo "---> Creating non existing secrets"
 
-              # Clean and extract secret name
-              echo "---------------------------------"
-              echo "Found secret $filename"
-              secret_name="${filename//.json}"
-              secret_name="${secret_name//"./$PULSAR_SECRETS_FOLDER/"}"
-              echo "Secret name is $secret_name"
-              existing_secret=$(gcloud secrets list --filter=$secret_name)
-              echo "Found $existing_secret"
+      # -- -- Loop secret folder and load files content into secret manager
+      for filename in ../$PULSAR_SECRETS_FOLDER/*$PULSAR_SECRETS_EXT; do
+          [ -e "$filename" ] || continue
 
-              # If value secret doesn't exist create it
-              if [[ $existing_secret == "" ]]; then
-                echo "--> Create new secret with name $secret_name"
-                gcloud secrets create $secret_name --replication-policy="automatic"
-                gcloud secrets versions add $secret_name --data-file=$filename
-              else
-                echo "--> The secret exist, you can update automatically the secret in next step."
-              fi
-              echo "---------------------------------"
-          done
+          # Clean and extract secret name
+          echo "---------------------------------"
+          echo "Found secret $filename"
+          secret_name="${filename//.json}"
+          secret_name="${secret_name//"../$PULSAR_SECRETS_FOLDER/"}"
+          echo "Secret name is $secret_name"
+          existing_secret=$(gcloud secrets list --filter=$secret_name)
+          echo "Found $existing_secret"
+
+          # If value secret doesn't exist create it
+          if [[ $existing_secret == "" ]]; then
+            echo "--> Create new secret with name $secret_name"
+            gcloud secrets create $secret_name --replication-policy="automatic"
+            gcloud secrets versions add $secret_name --data-file=$filename
+          else
+            echo "--> The secret exist, you can update automatically the secret in next step."
+          fi
+          echo "---------------------------------"
+      done
     fi
     # -- -- -- C2 - Update existing secrets if exist
     read -p "--->>>> Do you want to create new version of existing secrets in Secret Manager? [Y/y or N/n]: " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
-          echo "---> Creating new version of existing secrets"
-          # -- -- Loop secret folder and load files content into secret manager
-          for filename in ./$PULSAR_SECRETS_FOLDER/*$PULSAR_SECRETS_EXT; do
-              [ -e "$filename" ] || continue
+      pwd
+      echo "---> Creating new version of existing secrets"
+      # -- -- Loop secret folder and load files content into secret manager
+      for filename in ../$PULSAR_SECRETS_FOLDER/*$PULSAR_SECRETS_EXT; do
+          [ -e "$filename" ] || continue
 
-              # Clean and extract secret name
-              echo "---------------------------------"
-              echo "Found secret $filename"
-              secret_name="${filename//.json}"
-              secret_name="${secret_name//"./$PULSAR_SECRETS_FOLDER/"}"
+          # Clean and extract secret name
+          echo "---------------------------------"
+          echo "Found secret $filename"
+          secret_name="${filename//.json}"
+          secret_name="${secret_name//"../$PULSAR_SECRETS_FOLDER/"}"
 
-              # If value secret doesn't exist create it
-              gcloud secrets versions add $secret_name --data-file=$filename
+          # If value secret doesn't exist create it
+          gcloud secrets versions add $secret_name --data-file=$filename
 
-              echo "---------------------------------"
-          done
+          echo "---------------------------------"
+      done
     fi
 
   else
-      echo "---> The existing Cloud Secret Manager items will be used"
+    echo "---> The existing Cloud Secret Manager items will be used"
   fi
 
   #--  --  --  --  --  -- D -  Deploy Cloud Function --  --  --  --  --  -- --  --  --  --  --  -- --  -- --
@@ -184,43 +190,45 @@ then
   echo
   if [[ $REPLY =~ ^[Yy]$ ]]
   then
-      #Trying to delete it if cloud function if exist
-      echo "---> Trying to delete existing cloud function... "
-      gcloud beta functions delete "$PULSAR_NAME"  \
-      --gen2 \
-      --region="$REGION"
-      echo "---> The old Cloud function is deleted"
+    pwd
+    #Trying to delete it if cloud function if exist
+    echo "---> Trying to delete existing cloud function... "
+    gcloud beta functions delete "$PULSAR_NAME"  \
+    --gen2 \
+    --region="$REGION"
+    echo "---> The old Cloud function is deleted"
 
-      #------------  Deploy function
-      read -p "--> Do you want to create a new Cloud Function? [Y/y or N/n]: " -n 1 -r
-      echo
-      if [[ $REPLY =~ ^[Yy]$ ]]
-      then
-          echo "---> Creating the Cloud function with name: $PULSAR_NAME region:$PULSAR_REGION entrypoint:$PULSAR_ENTRY_POINT memory:$PULSAR_MEMORY runtime:$PULSAR_RUNTIME source:$PULSAR_GCS_SOURCE_PATH"
-          gcloud beta functions deploy "$PULSAR_NAME" \
-                                  --gen2 \
-                                  --region="$REGION" \
-                                  --service-account="$PULSAR_SERVICE_ACCOUNT" \
-                                  --entry-point="$PULSAR_ENTRY_POINT" \
-                                  --memory="$PULSAR_MEMORY" \
-                                  --runtime="$PULSAR_RUNTIME" \
-                                  --source="$PULSAR_GCS_SOURCE_PATH" \
-                                  --trigger-topic="$PULSAR_TOPIC" \
-                                  --timeout="$PULSAR_TIMEOUT" \
-                                  --min-instances="$PULSAR_MIN_INSTANCE" \
-                                  --max-instances="$PULSAR_MAX_INSTANCE" \
-                                  --no-allow-unauthenticated
+    #------------  Deploy function
+    read -p "--> Do you want to create a new Cloud Function? [Y/y or N/n]: " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      pwd
+      echo "---> Creating the Cloud function with name: $PULSAR_NAME region:$PULSAR_REGION entrypoint:$PULSAR_ENTRY_POINT memory:$PULSAR_MEMORY runtime:$PULSAR_RUNTIME source:$PULSAR_GCS_SOURCE_PATH"
+      gcloud beta functions deploy "$PULSAR_NAME" \
+                              --gen2 \
+                              --region="$REGION" \
+                              --service-account="$PULSAR_SERVICE_ACCOUNT" \
+                              --entry-point="$PULSAR_ENTRY_POINT" \
+                              --memory="$PULSAR_MEMORY" \
+                              --runtime="$PULSAR_RUNTIME" \
+                              --source="$PULSAR_GCS_SOURCE_PATH" \
+                              --trigger-topic="$PULSAR_TOPIC" \
+                              --timeout="$PULSAR_TIMEOUT" \
+                              --min-instances="$PULSAR_MIN_INSTANCE" \
+                              --max-instances="$PULSAR_MAX_INSTANCE" \
+                              --no-allow-unauthenticated
 
 
-          echo "---> The new cumulus Cloud function is accessible on https://console.cloud.google.com/functions/details/$REGION/$PULSAR_NAME?project=$PROJECT_ID"
-      fi
+      echo "---> The new Pulsar Cloud function is accessible on https://console.cloud.google.com/functions/details/$REGION/$PULSAR_NAME?project=$PROJECT_ID"
+    fi
 
-      echo "---> Trying to remove cloud function zip from the bucket and local folder... "
-      gsutil rm $PULSAR_GCS_SOURCE_PATH
-      # Removing the archive
-      rm "../$PULSAR_ZIP"
+    echo "---> Trying to remove cloud function zip from the bucket and local folder... "
+    gsutil rm $PULSAR_GCS_SOURCE_PATH
+    # Removing the archive
+    rm "../$PULSAR_ZIP"
   else
-      echo "---> The existing cloud function is not deleted"Test
+    echo "---> The existing cloud function is not deleted"Test
   fi
 
   #------------------- E - Deploy Cloud Scheduler Sample  ---------------------------------------------------------
@@ -228,25 +236,27 @@ then
   echo
   if [[ $REPLY =~ ^[Yy]$ ]]
   then
-      echo "---> Loading Cloud Scheduler sample configurations in path $PULSAR_TASKS_FOLDER/$PULSAR_TASK_SAMPLE"
-      MESSAGE_BODY=`cat "../$PULSAR_TASKS_FOLDER/$PULSAR_TASK_SAMPLE"`
-      echo "$MESSAGE_BODY"
+    pwd
+    echo "---> Loading Cloud Scheduler sample configurations in path $PULSAR_TASKS_FOLDER/$PULSAR_TASK_SAMPLE"
+    MESSAGE_BODY=`cat "../$PULSAR_TASKS_FOLDER/$PULSAR_TASK_SAMPLE"`
+    echo "$MESSAGE_BODY"
 
 
-      #---------------- - Try to delete scheduler if exist and create new one
-      echo "---> Trying to delete existing scheduler... "
-      gcloud beta scheduler jobs delete "$PULSAR_TASK_SAMPLE_NAME"
-
-      #Create scheduler
-      echo "---> Scheduling the task JSON with the cron $PULSAR_TASK_SAMPLE_CRON"
-      gcloud beta scheduler jobs create pubsub "$PULSAR_TASK_SAMPLE_NAME" \
-                                              --location="$REGION" \
-                                              --schedule="$PULSAR_TASK_SAMPLE_CRON" \
-                                              --topic="$PULSAR_TOPIC" \
-                                              --message-body="$MESSAGE_BODY"
+    #---------------- - Try to delete scheduler if exist and create new one
+    echo "---> Trying to delete existing scheduler... "
+    # Delete old sample if exist
+    gcloud beta scheduler jobs delete "$PULSAR_TASK_SAMPLE_NAME" \
+                                      --location="$REGION" \
+    # Create scheduler
+    echo "---> Scheduling the task JSON with the cron $PULSAR_TASK_SAMPLE_CRON"
+    gcloud beta scheduler jobs create pubsub "$PULSAR_TASK_SAMPLE_NAME" \
+                                            --location="$REGION" \
+                                            --schedule="$PULSAR_TASK_SAMPLE_CRON" \
+                                            --topic="$PULSAR_TOPIC" \
+                                            --message-body="$MESSAGE_BODY"
 
   else
-      echo "---> The Cloud Scheduler sample is not deployed"
+    echo "---> The Cloud Scheduler sample is not deployed"
   fi
 
   #------------------------------ F - Create default tables schema for Pulsar BigQuery------------------
@@ -254,34 +264,34 @@ then
   echo
   if [[ $REPLY =~ ^[Yy]$ ]]
   then
-      current_table_date=$(date +"%Y%m%d")
+    pwd
+    current_table_date=$(date +"%Y%m%d")
 
-      # Create the Pulsar dataset if not exist
-      echo "---> Creating the BigQuery dataset $PULSAR_NAME if not exist"
-      bq --location="$REGION" mk -d \
-      --description "Pulsar analytical logs." \
-      "$PULSAR_NAME"
+    # Create the Pulsar dataset if not exist
+    echo "---> Creating the BigQuery dataset $PULSAR_NAME if not exist"
+    bq --location="$REGION" mk -d \
+    --description "Pulsar analytical logs." \
+    "$PULSAR_NAME"
 
-      # Create tasked default table if not exist
-      echo "---> Creating empty tasked tasks table if not exist"
-      bq mk --table --description "The Pulsar tasked tasks default table" "$PROJECT_ID:$PULSAR_NAME.tasked_$current_table_date" "$TASK_SCHEMA"
+    # Create tasked default table if not exist
+    echo "---> Creating empty tasked tasks table if not exist"
+    bq mk --table --description "The Pulsar tasked tasks default table" "$PROJECT_ID:$PULSAR_NAME.tasked_$current_table_date" "$TASK_SCHEMA"
 
-      # Create initiated default table if not exist
-      echo "---> Creating empty initiated tasks table if not exist"
-      bq mk --table --description "The Pulsar tasks initiated default table" "$PROJECT_ID:$PULSAR_NAME.initiated_$current_table_date" "$TASK_SCHEMA"
+    # Create initiated default table if not exist
+    echo "---> Creating empty initiated tasks table if not exist"
+    bq mk --table --description "The Pulsar tasks initiated default table" "$PROJECT_ID:$PULSAR_NAME.initiated_$current_table_date" "$TASK_SCHEMA"
 
-      # Create processed default table if not exist
-      echo "---> Creating empty processed tasks table if not exist"
-      bq mk --table --description "The Pulsar processed tasks default table" "$PROJECT_ID:$PULSAR_NAME.processed_$current_table_date" "$TASK_SCHEMA"
+    # Create processed default table if not exist
+    echo "---> Creating empty processed tasks table if not exist"
+    bq mk --table --description "The Pulsar processed tasks default table" "$PROJECT_ID:$PULSAR_NAME.processed_$current_table_date" "$TASK_SCHEMA"
 
-      # Create terminated default table if not exist
-      echo "---> Creating empty terminated tasks table if not exist"
-      bq mk --table --description "The Pulsar terminated tasks default table" "$PROJECT_ID:$PULSAR_NAME.terminated_$current_table_date" "$TASK_SCHEMA"
+    # Create terminated default table if not exist
+    echo "---> Creating empty terminated tasks table if not exist"
+    bq mk --table --description "The Pulsar terminated tasks default table" "$PROJECT_ID:$PULSAR_NAME.terminated_$current_table_date" "$TASK_SCHEMA"
 
   else
-      echo "---> The deployment don't create default Pulsar BigQuery tables"
+    echo "---> The deployment don't create default Pulsar BigQuery tables"
   fi
-
 
 else
   echo "Operation is aborted"

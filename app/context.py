a48cbdd8454fc5142806c99a9725f5390a274114
Tiyag K. G <tiyabkg@gmail.com>
2022-06-03 06:07:01 +0200

    Remove context from git since it's autogenerate

--- app/context.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-APP_NAME = "pulsar"
-RUNTIME = "python39"
-PROJECT_ID = "gcpbees-test"
-REGION = "europe-west1"
-SERVICE_ACCOUNT_EMAIL = "pulsar@gcpbees-test.iam.gserviceaccount.com"
-TOPIC = "pulsar-topic"
-STORAGE = "gcpbees-test-pulsar"
-DATASET = "pulsar"

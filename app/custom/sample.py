572f84cf45ad0cd5688ca6e9e91f669a0f31aaa4
Tiyag K. G <tiyabkg@gmail.com>
2022-06-07 06:37:56 +0200

    Update samples

--- app/custom/sample.py
+++ app/custom/sample.py
@@ -1,5 +1,5 @@
 import time
-
+import base64
 
 class Greeting(object):
     def __init__(self):
@@ -7,14 +7,22 @@ class Greeting(object):
 
     @staticmethod
     def get(name):
-        return "Get Hello {}".format(name)
+        return Greeting.response("From sample 1: Get Hello {}".format(name))
 
     @staticmethod
     def say(message):
-        return "Say {}".format(message)
+        return Greeting.response("From sample 1: Say {}".format(message))
 
     @staticmethod
     def sleeper(sec):
         time.sleep(int(sec))
-        return "Process waited {} seconds.".format(sec)
+        return Greeting.response("From sample 1: Process waited {} seconds.".format(sec))
+
+    @staticmethod
+    def response(json_data):
+        # Encode the resource information before forwarding
+        data_bytes = json_data.encode("utf-8")
+        encoded_data = base64.b64encode(data_bytes)
+        decoded_data = encoded_data.decode("utf-8")
+        return decoded_data
 
